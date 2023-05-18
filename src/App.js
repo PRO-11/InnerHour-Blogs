d378853d868fe6c76e1294f2a9a7db2a72bdde98
PRATHAM GUPTA <91843874+PRO-11@users.noreply.github.com>
2023-05-18 22:42:36 +0530

    project done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,139 @@
-import logo from './logo.svg';
+import logo from './logo.png'
+import demo from './demo.jpg'
 import './App.css';
-
+import Card from './Card';
+import { useEffect, useState } from 'react';
+import axios from "axios"
 function App() {
+  const [data,setData]=useState([]);
+  //Pagination State
+  const [pageNo,setPageNo]=useState(1)
+  const [limit,setlimit]=useState(0)
+  //Modal State
+  const [modal,setmodal]=useState(false)
+  const [modelDetail,setDetail]=useState([])
+  //Search State
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredCards, setFilteredCards] = useState([]);
+
+  const handleSearch = (event) => {
+    const query = event.target.value.toLowerCase();
+    console.log(query)
+    setSearchQuery(query);
+
+    // Filter the cards based on the search query
+    const filtered = data.filter((card) =>
+      card.title.toLowerCase().includes(query)
+    );
+    console.log(filtered)
+    setFilteredCards(filtered);
+  };
+
+  const handlePrevious=()=>{
+    console.log("previous")
+    setPageNo(pageNo-1);
+  }
+  const handleNext=()=>{
+    console.log("next")
+    console.log(limit)
+    setPageNo(pageNo+1);
+  }
+
+  const openModal=async(slug)=>{
+    console.log(slug)
+    setmodal(true)
+    const response=await axios.get(`https://api.theinnerhour.com/v1/blogdetail/${slug}`)
+      console.log(response.data)
+      setDetail(response.data.blog)
+
+  }
+  useEffect(()=>{
+    const getApiData=async()=>{
+      const response=await axios.get(`https://api.theinnerhour.com/v1/customers/resources/articles/list?page=${pageNo}&limit=10`)
+      console.log(response.data.total_page)
+      setlimit(response.data.total_page)
+      setData(response.data.data)
+    }
+    getApiData()
+  },[pageNo])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class=" flex flex-col" >
+     
+     {modal&&<div id="defaultModal" tabindex="-1" aria-hidden="true" class="fixed   bg-black bg-opacity-50 top-0 left-0 right-0 z-50 flex justify-center   w-full p-11  overflow-x-hidden overflow-y-auto md:inset-0 h-full max-h-full ">
+      
+    <div class="relative w-full max-w-3xl max-h-full flex flex-col  ">
+    <button type="button" class="mb-3   bg-gray-700 rounded-full text-sm p-1.5 ml-auto inline-flex flex-row-reverse items-center text-white" data-modal-hide="defaultModal" onClick={()=>setmodal(false)}>
+                    <svg aria-hidden="true" class="w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            
+            <div class="flex items-start justify-between  border-b rounded-t dark:border-gray-600">
+               <img src={demo} class="rounded-t-lg w-full h-96"/>
+               
+            </div>
+          
+            <div class="p-6 space-y-6">
+              <h1 class="  text-black font-extrabold text-xl">{modelDetail?.title}</h1>
+                <p class="text-base text-black font-normal" dangerouslySetInnerHTML={{ __html:modelDetail?.body }}>
+                    
+                </p>
+            </div>
+          
+        
+        </div>
+    </div>
+</div>}
+      <div >
+        <img src={logo} class="mx-28 my-3 h-12" />
+      </div>
+      <div class="h-full  md:w-full w-max " style={{"backgroundColor":"#f5f5f5"}}>
+      <div class="flex  flex-row md:justify-between  ">
+        {(searchQuery.trim().length==0)&&<h3 class="mx-28 my-14 font-semibold text-xl">All articles</h3>}
+        {(searchQuery.trim().length!=0)&&<h3 class="mx-28 my-14 font-semibold text-xl w-auto ">Search results for:{searchQuery}</h3>}
+        <div class="flex items-center justify-center  ">
+        <div class="relative text-gray-600">
+      <input type="text" name="q" value={searchQuery}
+        onChange={handleSearch} class="md:w-80 w-1/2 p-3 pl-5 mx-16 text-sm text-black rounded-md  focus:outline-none focus:bg-white " placeholder="Search articles" autocomplete="off"/>
+      <span class="absolute inset-y-0 right-0 pt-2 pr-24 md:pr-16  flex-row-reverse">
+        <button type="submit" class="p-1 focus:outline-none focus:shadow-outline">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-6 h-6"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+        </button>
+      </span>
+    </div>
+      </div>
+      </div>
+      <div className=" mx-24  mt-5 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3  gap-1 ">
+      {(searchQuery.trim().length==0)&&data.map((ele)=>{
+        return <Card key={ele.id} title={ele.title} description={ele.short_description} slug={ele.slug} openModal={openModal}/>
+      })}
+      </div>
+      {(filteredCards.length>0&&searchQuery.trim().length>0)?<div className=" mx-24  mt-5 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 place-items-center gap-1 ">
+      {filteredCards.map((ele)=>{
+        return <Card key={ele.id} title={ele.title} description={ele.short_description} slug={ele.slug} openModal={openModal}/>
+      })}
+      </div>:
+      searchQuery.trim().length!=0&&<p class="text-center text-lg font-bold">No results for this search</p>
+
+      }
+      <div className="flex justify-between sm:justify-end items-center mt-10 gap-1">
+      {(searchQuery.trim().length==0)&&<button disabled={pageNo<=1} className={`${
+                  pageNo > 1
+                    ? "hover:bg-gray-400 hover:text-white"
+                    : "opacity-50 cursor-not-allowed"
+                } mx-3 my-3 w-fit  flex justify-center items-center text-lg text-center font-semibold px-3 py-1 border-2  rounded-lg text-gray-800] `} onClick={handlePrevious}>
+      &larr;  Prev
+      </button>}
+       {(searchQuery.trim().length==0)&&<button disabled={pageNo== limit}  className={`${
+                  pageNo< limit
+                    ? "hover:bg-gray-400 hover:text-white"
+                    : "opacity-50 cursor-not-allowed"
+                } mx-3 my-3 w-fit  flex justify-center items-center text-lg text-center font-semibold px-3 py-1 border-2  rounded-lg text-gray-800] `} onClick={handleNext}>
+         Next &rarr;
+      </button>}
+  </div>
+      </div>
+       
     </div>
   );
 }
---
+++ src/Card.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Iron from './iron.jpg'
+import { Link } from 'react-router-dom'
+function Card({title,description,slug,openModal}) {
+
+  return (
+    
+    <div
+    style={{ boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px" }}
+    className="border   bg-gray-50 scale-90  xs:scale-100 w-[350px] md:w-full  min-[1500px]:w-[340px!important] h-[350px] flex flex-col items-center justify-between rounded-xl overflow-hidden hover:-translate-y-1 hover:scale-95 xs:hover:scale-[1.025] transition-transform"
+  >
+    <a onClick={()=>openModal(slug)}>
+        <img class="rounded-t-lg w-full h-36" src={Iron} alt=""  />
+
+        <div
+        className=" z-[5] my-5 mx-3  w-full h-64 flex flex-col justify-between items-start p-3 pt-0 bg-gray-50"
+      >
+        <div className="w-full flex flex-col justify-between items-start overflow-hidden ">
+            <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
+       
+        <h3 class="mb-3 my-3  text-black-100 font-medium">{(title.length+description.length)>150?description.substring(0,120)+"...":description}</h3>
+      </div>
+      </div>
+      </a>
+   
+</div>  
+
+
+   
+
+  )
+}
+
+export default Card
